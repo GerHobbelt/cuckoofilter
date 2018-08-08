a1833397b443f412ecbc8d3a5f91babf66f2727e
Thomas Mueller <mueller@adobe.com>
2018-08-08 08:58:46 +0200

    Added Golomb Compressed Sequence implementation (slow as expected)

--- benchmarks/bulk-insert-and-query.cc
+++ benchmarks/bulk-insert-and-query.cc
@@ -56,6 +56,7 @@
 #include "cuckoofilter.h"
 #include "xorfilter.h"
 #include "bloom.h"
+#include "gcs.h"
 #include "gqf_cpp.h"
 #include "random.h"
 #include "simd-block.h"
@@ -66,6 +67,7 @@ using namespace std;
 using namespace cuckoofilter;
 using namespace xorfilter;
 using namespace bloomfilter;
+using namespace gcsfilter;
 using namespace gqfilter;
 
 // The number of items sampled when determining the lookup performance
@@ -176,6 +178,20 @@ struct FilterAPI<XorFilter<ItemType, bits_per_item>> {
 };
 
 template <typename ItemType, size_t bits_per_item>
+struct FilterAPI<GcsFilter<ItemType, bits_per_item>> {
+  using Table = GcsFilter<ItemType, bits_per_item>;
+  static Table ConstructFromAddCount(size_t add_count) { return Table(add_count); }
+  static void Add(uint64_t key, Table* table) {
+  }
+  static void AddAll(const vector<ItemType> keys, const size_t start, const size_t end, Table* table) {
+    table->AddAll(keys, start, end);
+  }
+  static bool Contain(uint64_t key, const Table * table) {
+    return (0 == table->Contain(key));
+  }
+};
+
+template <typename ItemType, size_t bits_per_item>
 struct FilterAPI<GQFilter<ItemType, bits_per_item>> {
   using Table = GQFilter<ItemType, bits_per_item>;
   static Table ConstructFromAddCount(size_t add_count) { return Table(add_count); }
@@ -269,6 +285,12 @@ int main(int argc, char * argv[]) {
   cout << StatisticsTableHeader(NAME_WIDTH, 5) << endl;
 
   auto cf = FilterBenchmark<
+      GcsFilter<uint64_t, 8>>(
+      add_count, to_add, to_lookup);
+
+  cout << setw(NAME_WIDTH) << "GCS" << cf << endl;
+
+  cf = FilterBenchmark<
       BloomFilter<uint64_t, 10 /* bits per item */>>(
       add_count, to_add, to_lookup);
 
